e15e1a7e6d20f860451765442228c4c0c4274755
Andrew Salmanov <ads739912@gmail.com>
2023-09-08 16:39:13 +0300

    sidebar corrections

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,11 @@
 function dropDown() {
-        document.querySelector('navbar_def_dropdown_btn').classList.toggle("show");
+        document.getElementById('Dropdown').classList.toggle("show");
 }
 
 window.onclick = function (event) {
         if (!event.target.matches('.navbar_def_dropdown_btn')) {
-                let dropdowns = document.getElementsByClassName("navbar_def_dropdown_btn_content");
+                
+                let dropdowns = document.getElementsByClassName("navbar_def_dropdown_content");
                 let i;
                 for (i = 0; i < dropdowns.length; i++) {
                         let openDropdown = dropdowns[i];
@@ -14,3 +15,4 @@ window.onclick = function (event) {
                 }
         }
 }
+
