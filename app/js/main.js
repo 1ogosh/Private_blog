886590bce6b0fa316ab94db17087483ab14991d5
Andrew Salmanov <ads739912@gmail.com>
2023-11-28 17:44:02 +0200

    backTotheRoots

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,15 @@
 function dropDown() {
-        document.getElementById('Dropdown').classList.toggle("show");
+        document.getElementById('Dropdown').classList.toggle('hide');
 }
 
 window.onclick = function (event) {
-        if (!event.target.matches('.navbar_def_dropdown_btn')) {
-                
-                let dropdowns = document.getElementsByClassName("navbar_def_dropdown_content");
+        if (!event.target.matches('navbar_def_dropdown_btn')) {
+                let dropdowns = document.getElementsByClassName('navbar_def_dropdown_content');
                 let i;
                 for (i = 0; i < dropdowns.length; i++) {
                         let openDropdown = dropdowns[i];
-                        if (openDropdown.classList.contains('show')) {
-                                openDropdown.classList.remove('show');
+                        if (openDropdown.classList.contains('hide')) {
+                                openDropdown.classList.remove('hide');
                         }
                 }
         }
