0da1b4ffecd6eb84eda44c09a245555228ece81a
Andrew Salmanov <ads739912@gmail.com>
2023-11-29 15:31:32 +0200

    corrections

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,20 @@
 function dropDown() {
-        document.getElementById('Dropdown').classList.toggle('hide');
+        document.getElementById('Dropdown').classList.toggle('.hide');
 }
 
 window.onclick = function (event) {
         if (!event.target.matches('navbar_def_dropdown_btn')) {
-                let dropdowns = document.getElementsByClassName('navbar_def_dropdown_content');
+                let dropdowns = document.getElementsByClassName('.navbar_def_dropdown_content');
                 let i;
                 for (i = 0; i < dropdowns.length; i++) {
                         let openDropdown = dropdowns[i];
-                        if (openDropdown.classList.contains('hide')) {
-                                openDropdown.classList.remove('hide');
+                        if (openDropdown.classList.contains('.hide')) {
+                                openDropdown.classList.remove('.hide');
+                        } else {
+                                openDropdown.classList.add('.hide');
                         }
                 }
         }
 }
+
 
