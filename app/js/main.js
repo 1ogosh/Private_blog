e40491f54d3025a01d6a5a7fe61dfde87b3947b7
Andrew Salmanov <ads739912@gmail.com>
2023-12-18 12:36:13 +0200

    finished dropdown button

--- app/js/main.js
+++ app/js/main.js
@@ -2,15 +2,20 @@
 
 function dropdown() {
   const selectItem = document.querySelectorAll(".navbar_def_dropdown_btn");
+  const selectBtn = document.querySelectorAll(".navbar_def_dropdown_btn_img");
+
 
   selectItem.forEach((item, i) => {
     item.addEventListener("click", (event) => {
       const selectBody = document.querySelectorAll(".navbar_def_dropdown_content");
 
+
+      selectBtn[i].classList.toggle("rotate");
       selectBody[i].classList.toggle("show");
       item.classList.toggle("shadow");
     });
   });
+
 }
 
 dropdown();
