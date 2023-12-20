733fafe7eaf1f29d2a66a7a91068d1b9ff52018b
Andrew Salmanov <ads739912@gmail.com>
2023-12-20 17:44:59 +0200

    correct fileSelection function

--- app/js/main.js
+++ app/js/main.js
@@ -21,3 +21,45 @@ function dropdown() {
 dropdown();
 
 
+///File selection
+
+window.URL = window.URL || window.webkitURL;
+
+let fileSelect = document.getElementsByClassName("text-field__aicon_1"),
+  fileElem = document.getElementById("fileElem"),
+  fileList = document.getElementById("fileList");
+
+fileSelect.addEventListener("click", function (e) {
+  if (fileElem) {
+    fileElem.click();
+  }
+  e.preventDefault();
+},
+  false,
+);
+
+function handleFiles(files) {
+  if(!files.lengh) {
+    fileList.innerHTML = "<p>No files selected!</p>";
+  }else{
+    let list = document.createElement("ul");
+    for (let i = 0; i < file.length; i++){
+      let li = document.createElement("li");
+      list.appendChild(li);
+      
+      let img = document.createElement("img");
+      img.src = window.URL.createObjectURL(files[i]);
+      img.height = 60;
+      img.onload = function() {
+        window.URL.revokeObjectURL(this.src);
+      };
+      li.appendChild(img);
+      let info = document.createElement("span");
+          info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+          li.appendChild(info);
+    }
+  }
+}
+
+
+
